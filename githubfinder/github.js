16f8238900e9d8efe284fafe26d554928702caeb
AurimasUrbonas <66870338+AurimasUrbonas@users.noreply.github.com>
2020-06-13 16:15:42 +0300

    Add files via upload

---
+++ githubfinder/github.js
@@ -0,0 +1,22 @@
+class GitHub {
+  consturctor() {
+    this.client_id = '8db0c1ef374d8f756a18';
+    this.client_secret = 'bb1827dd7672c14b583cd660f8e409557255c0fc';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
